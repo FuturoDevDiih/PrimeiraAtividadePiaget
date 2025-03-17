6f6e2ad624887fb4209e37a58c8077065456ca67
FuturoDevDiih <dih.lolo2015@gmail.com>
2025-03-17 19:26:59 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiagetNovo/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace WebPiagetNovo.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public String? Nome { get; set; }
+        public String? Escola { get; set; }
+        public String? Pai { get; set; }
+        public String? Mae{ get; set; }
+        public String? RGM { get; set; }
+        public DateTime Idade { get; set; }
+        public decimal Notas { get; set; }
+        public String? Professor { get; set; }
+    }
+}
